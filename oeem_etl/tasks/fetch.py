6e1155ab76ca9fa18ee41e6ee1ffa8e8a8e84689
Juan-Pablo Velez <jpvelez@gmail.com>
2016-04-27 01:07:04 -0400

    Rewrite fetch tasks

--- oeem_etl/tasks/fetch.py
+++ oeem_etl/tasks/fetch.py
@@ -1,31 +1,39 @@
-import luigi
-from luigi.s3 import S3Target, S3Client
-from luigi.format import MixedUnicodeBytesFormat
+from ..fetchers import fetch_all_customer_usage
 
-class FetchToS3(luigi.Task):
-
-    s3_key = luigi.Parameter()
-    fetch_callable = luigi.Parameter()
-    s3_profile = luigi.Parameter()
-
-    def run(self):
-        with self.output().open('w') as target:
-            target.write(self.fetch_callable())
-
-    def output(self):
-        return S3Target(self.s3_key, format=MixedUnicodeBytesFormat(), client=self._s3_client())
-
-    def _s3_client(self):
-        return S3Client(profile_name=self.s3_profile)
-
-class FetchToLocal(luigi.Task):
-
-    filepath = luigi.Parameter()
-    fetch_callable = luigi.Parameter()
+class SaveCustomerUsage(luigi.Task):
+    project_id = luigi.Parameter()
+    usage_point_category = luigi.Parameter()
+    min_date = luigi.DateParameter()
+    max_date = luigi.DateParameter()
+    xml = luigi.Parameter()
 
     def run(self):
         with self.output().open('w') as target:
-            target.write(self.fetch_callable())
+            target.write(self.xml)
 
     def output(self):
-        return luigi.LocalTarget(self.filepath)
+        filename = '{}_{}_{}_{}.xml'.format(self.project_id
+                                            self.usage_point_category,
+                                            self.min_date.for_json(),
+                                            self.max_date.for_json())
+        # TODO: make base work with  target factory.
+        return local.Target('data/consumption/raw/' + filename)
+
+class FetchAllCustomers(luigi.WrapperTask):
+    config = luigi.Parameter()
+    customers = luigi.Parameter()
+    target = luigi.Parameter()
+
+    def requires(self):
+        # TODO refactor fetch_all_customer_usage into a class?
+        tasks = []
+        for customer in customers:
+            for up_cat, min_date, max_date, usage_xml in fetch_all_customer_usage(customer['subscription_id'],
+                                                                                  customer['active_access_token'],
+                                                                                  # TODO pass this in
+                                                                                  self.espi):
+                tasks.append(SaveCustomerUsage(customer['project_id'],
+                                               up_cat, min_date,
+                                               max_date, usage_xml))
+
+        return tasks
